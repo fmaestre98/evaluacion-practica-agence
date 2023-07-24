22ef1d3dfcbef2ce8858715b01c13b2a6182a10a
Fabian Ortiz Maestre <fmaestre98@gmail.com>
2023-07-24 13:51:53 -0400

    update compose dependencies an styles

--- resources/views/livewire/show-comercial-data.blade.php
+++ resources/views/livewire/show-comercial-data.blade.php
@@ -11,9 +11,12 @@
         <livewire:comercial-form :cao_usuarios_selected="$cao_usuarios_selected" :cao_usuarios_unselected="$cao_usuarios_unselected" :wire:key="uniqid()" />
         <div wire:ignore>
 
-        <div wire:loading.delay.longer class="loading">
-                        <div class="spinner-border" role="status">
-                                <span class="visually-hidden">Loading...</span>
+
+                <div wire:loading.delay.longer class="overlay">
+                        <div class="d-flex justify-content-center align-items-center" style="height:100vh">
+                                <div class="spinner-grow text-primary" style="width: 4rem; height: 4rem;" role="status">
+                                        <span class="visually-hidden">Loading...</span>
+                                </div>
                         </div>
                 </div>
 
