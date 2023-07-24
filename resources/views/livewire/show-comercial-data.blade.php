ab1a165ac35cb030a3dcb73f72d092410f39f43a
Fabian Ortiz Maestre <fmaestre98@gmail.com>
2023-07-24 12:21:32 -0400

    added spinner loading to views

--- resources/views/livewire/show-comercial-data.blade.php
+++ resources/views/livewire/show-comercial-data.blade.php
@@ -1,9 +1,9 @@
 <div id="show_comercial">
         <div id="date_interval">
                 <p>Período:</p>
-                <input type="text" wire:model="periodo_start" class="form-control" name="datepicker_start" id="datepicker_start" readonly />
+                <input type="text" wire:model.debounce.1000ms="periodo_start" class="form-control" name="datepicker_start" id="datepicker_start" readonly />
                 <p>a</p>
-                <input type="text" wire:model="periodo_end" class="form-control" name="datepicker_end" id="datepicker_end" readonly />
+                <input type="text" wire:model.debounce.1000ms="periodo_end" class="form-control" name="datepicker_end" id="datepicker_end" readonly />
 
         </div>
 
@@ -16,16 +16,32 @@
                 @foreach($cao_usuarios_selected as $usuario)
                 <livewire:relatorio-table :usuario="$usuario" :start="$periodo_start" :end="$periodo_end" :wire:key="uniqid()" />
                 @endforeach
-
+                <div wire:loading>
+                        <div class="spinner-border" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                        </div>
+                </div>
                 @endif
 
                 @if($showGrafico)
                 <livewire:bar-chart :co_usuarios=$cao_usuarios_selected :start="$periodo_start" :end="$periodo_end" :wire:key="uniqid()" />
+                <div wire:loading>
+                        <div class="spinner-border" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                        </div>
+                </div>
                 @endif
 
                 @if($showPizza)
                 <livewire:pie-chart :co_usuarios=$cao_usuarios_selected :start="$periodo_start" :end="$periodo_end" :wire:key="uniqid()" />
+                <div wire:loading>
+                        <div class="spinner-border" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                        </div>
+                </div>
                 @endif
+
+
         </div>
 
 </div>
