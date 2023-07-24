7a5f09b5e46426f093ec30cbdbf62b4a2d7ac260
Fabian Ortiz Maestre <fmaestre98@gmail.com>
2023-07-24 14:42:21 -0400

    change delay duration

--- resources/views/livewire/comercial-form.blade.php
+++ resources/views/livewire/comercial-form.blade.php
@@ -1,5 +1,5 @@
 <div class="col-md-12">
-    
+
     <h3>Consultores:</h3>
     <div id="selects_container">
         <select wire:model="selectedItemsModel" class="form-select" id="first_list" size="6" multiple aria-label="multiple select example">
@@ -11,10 +11,17 @@
         <div id="buttons_container">
             <button wire:click="getSelectedItems" type="button" id="button_select" class="btn btn-secondary btn-sm"> <i class="bi bi-box-arrow-in-right"></i></button>
             <button wire:click="getUnSelectedItems" type="button" id="button_desselect" class="btn btn-secondary btn-sm"> <i class="bi bi-box-arrow-in-left"></i></button>
+            <div wire:loading.delay.long class="overlay">
+                <div class="d-flex justify-content-center align-items-center" style="height:100vh">
+                    <div class="spinner-grow text-primary" style="width: 4rem; height: 4rem;" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+            </div>
         </div>
         <select wire:model="unSelectedItemsModel" class="form-select" id="selected" size="6" multiple aria-label="multiple select example">
             @foreach($cao_usuarios_selected as $usuario)
-             <option value="{{ $usuario['co_usuario']}}">{{$usuario['no_usuario']}}</option>
+            <option value="{{ $usuario['co_usuario']}}">{{$usuario['no_usuario']}}</option>
             @endforeach
         </select>
 
@@ -22,6 +29,13 @@
             <button wire:click="showRelatorio" type="button" class="btn btn-primary"><i class="bi bi-clipboard-data-fill"></i> Relátorio</button>
             <button wire:click="showGrafico" type="button" class="btn btn-info"><i class="bi bi-bar-chart-line-fill"></i> Gráfico</button>
             <button wire:click="showPizza" type="button" class="btn btn-success"><i class="bi bi-pie-chart-fill"></i> Pizza</button>
+            <div wire:loading.delay.long class="overlay">
+                <div class="d-flex justify-content-center align-items-center" style="height:100vh">
+                    <div class="spinner-grow text-primary" style="width: 4rem; height: 4rem;" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
--- resources/views/livewire/show-comercial-data.blade.php
+++ resources/views/livewire/show-comercial-data.blade.php
@@ -4,21 +4,18 @@
                 <input type="text" wire:model.debounce.1000ms="periodo_start" class="form-control" name="datepicker_start" id="datepicker_start" readonly />
                 <p>a</p>
                 <input type="text" wire:model.debounce.1000ms="periodo_end" class="form-control" name="datepicker_end" id="datepicker_end" readonly />
-
-        </div>
-
-
-        <livewire:comercial-form :cao_usuarios_selected="$cao_usuarios_selected" :cao_usuarios_unselected="$cao_usuarios_unselected" :wire:key="uniqid()" />
-        <div wire:ignore>
-
-
-                <div wire:loading.delay.longer class="overlay">
+                <div wire:loading.delay.long class="overlay">
                         <div class="d-flex justify-content-center align-items-center" style="height:100vh">
                                 <div class="spinner-grow text-primary" style="width: 4rem; height: 4rem;" role="status">
                                         <span class="visually-hidden">Loading...</span>
                                 </div>
                         </div>
                 </div>
+        </div>
+
+
+        <livewire:comercial-form :cao_usuarios_selected="$cao_usuarios_selected" :cao_usuarios_unselected="$cao_usuarios_unselected" :wire:key="uniqid()" />
+        <div wire:ignore>
 
 
                 @if($showRelatorio)
